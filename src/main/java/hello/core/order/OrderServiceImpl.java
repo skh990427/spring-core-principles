77c6df778d5e5f77b03e65df897aa61bebd027a0
KwangHo <skh990427@naver.com>
2024-01-06 22:51:58 +0900

    refactor: Lombok 적용

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,14 +3,20 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
 //    @Autowired
 //    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
@@ -20,16 +26,11 @@ public class OrderServiceImpl implements OrderService {
 //    @Autowired
 //    public void setMemberRepository(MemberRepository memberRepository) {
 //        this.memberRepository = memberRepository;
+
 //    }
-
     //    @Autowired private DiscountPolicy discountPolicy;
-//    @Autowired private MemberRepository memberRepository;
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired private MemberRepository memberRepository;
 
 /*    @Autowired
     public void setMemberRepository(MemberRepository memberRepository) {
