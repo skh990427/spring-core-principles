e80045a7e86e80a89b4228da7b37a52459eafbf9
KwangHo <skh990427@naver.com>
2024-01-17 21:07:23 +0900

    InitializingBean, DisposableBean 빈 생명주기 초기화 및 소멸 인터페이스

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -27,14 +27,14 @@ public class NetworkClient implements InitializingBean, DisposableBean {
 
     //서비스 종료시 호출
     public void disconnect() {
-        System.out.println("close " + url);
+        System.out.println("close : " + url);
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
-        call("초기화 연결 메시지");
+        call("초기화 연결 메세지");
     }
 
     @Override
